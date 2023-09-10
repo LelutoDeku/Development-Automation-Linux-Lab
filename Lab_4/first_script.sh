9d2ec0a91e75c4bb5c4a33d034c268bf98fcea04
Hammad <hammadsiddiqui1212@gmail.com>
2023-09-10 16:12:17 +0530

    Lab 2 updated

--- Lab_4/first_script.sh
+++ Lab_4/first_script.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 function sum	{
-	if  [[ $1 =~ ^[0-9]+$ || $2 =~ ^[0-9]+$ ]]
+	if ! [[ $1 =~ ^[0-9]+$ && $2 =~ ^[0-9]+$ ]]
 	then
-		echo "Please enter integer value"
+		echo "Please enter integer value(s)"
 		exit 1
 	else	
-		s=(( $1 + $2 ))
+		s=$(( $1 + $2 ))
 		echo "Sum of argumnets is: $s"
 	fi
+	
 	}
+	
 sum $1 $2
